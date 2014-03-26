4f5566437b8068665211eb4422ae88dc49917f44
Julie <ju.ralph@gmail.com>
2014-03-26 16:55:14 -0700

    Showing issue with SafariDriver and number inputs

--- safariminus.js
+++ safariminus.js
@@ -7,9 +7,19 @@ var driver = new webdriver.Builder().
     withCapabilities({'browserName': 'safari'}).
     build();
 
-driver.get('http://juliemr.github.io/webdriverjs-bugs/');
+driver.get('http://juliemr.github.io/webdriver-bugs/');
 
-driver.findElement(webdriver.By.id('textinput')).sendKeys('-');
-driver.findElement(webdriver.By.id('textinput')).sendKeys(webdriver.Keys.MINUS);
+driver.findElement(webdriver.By.id('numberinput')).sendKeys('-');
+driver.findElement(webdriver.By.id('numberinput')).sendKeys(webdriver.Key.SUBTRACT);
+driver.findElement(webdriver.By.id('numberinput')).sendKeys('.');
+driver.findElement(webdriver.By.id('numberinput')).sendKeys('6');
+
+
+driver.sleep(5000);
+
+driver.findElement(webdriver.By.id('numberinput')).getAttribute('value').
+    then(function(value) {
+      console.log('value: ' + value);
+    });
 
 driver.quit();
